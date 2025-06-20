ba26f8e34335c6cdfefd0faa8fe9f6273b0dc3b3
Fabiolah Nirinasoa Xu Lin <fxu-lin@c1r12s2.42antananarivo.mg>
2025-06-20 15:43:37 +0300

    update wall x and wall y

--- src/mlx/action/ft_key_press.c
+++ src/mlx/action/ft_key_press.c
@@ -18,14 +18,14 @@ void	ft_render(t_mlx *mlx)
 	ft_color_display(mlx);
 }
 
-int ft_is_wall_x(t_mlx *mlx, int x)
+int ft_is_wall_y(t_mlx *mlx, int x)
 {
   if(mlx->map[x][mlx->y_player] == '1')
     return (1);
   return (0);
 }
 
-int ft_is_wall_y(t_mlx *mlx, int y)
+int ft_is_wall_x(t_mlx *mlx, int y)
 {
   if(mlx->map[mlx->x_player][y] == '1')
     return (1);
@@ -35,16 +35,15 @@ int ft_is_wall_y(t_mlx *mlx, int y)
 
 int	ft_key_press(int code, t_mlx *mlx)
 {
-    printf("Ito ilay code %d\n", code);
 	if (code == 65307)
 		ft_mlx_destroy(mlx);
-	else if (code == 100 && !ft_is_wall_y(mlx, mlx->y_player + 1))
+	else if (code == 100 && !ft_is_wall_x(mlx, mlx->y_player + 1))
 		mlx->y_player = mlx->y_player + 1;
-	else if (code == 97 && !ft_is_wall_y(mlx, mlx->y_player - 1))
+	else if (code == 97 && !ft_is_wall_x(mlx, mlx->y_player - 1))
 		mlx->y_player = mlx->y_player - 1;
-	else if (code == 119 && !ft_is_wall_x(mlx, mlx->x_player - 1))
+	else if (code == 119 && !ft_is_wall_y(mlx, mlx->x_player - 1))
 		mlx->x_player = mlx->x_player - 1;
-	else if (code == 115 && !ft_is_wall_x(mlx, mlx->x_player + 1))
+	else if (code == 115 && !ft_is_wall_y(mlx, mlx->x_player + 1))
 		mlx->x_player = mlx->x_player + 1;
   ft_render(mlx);
 	return (0);
