2d54ebeb07af8b7feac8168bc4924c2298541b48
Ivan Tahiana Rakotomanga <irakotom@c1r10s14.42antananarivo.mg>
2025-07-23 12:32:36 +0300

    fix the 32 in the function to argument

--- src/math/ft_wall.c
+++ src/math/ft_wall.c
@@ -38,6 +38,5 @@ double	ft_get_height_wall(t_player player, t_mlx mlx, double distance,
 	throw_distance = ft_throw_distance(mlx.width, player.fov);
 	if (distance == 0.0)
 		return (0);
-	wall_height = mlx.side;
 	return ((wall_height / distance) * throw_distance);
 }
