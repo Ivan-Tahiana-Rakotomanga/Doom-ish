fdb3a8c25b45a1c4ab1bfe3c9d72c866273959fb
Your Name <you@example.com>
2025-08-21 19:19:43 +0300

    modif speed and add textures

--- src/mlx/mlx_init/ft_init_player.c
+++ src/mlx/mlx_init/ft_init_player.c
@@ -32,8 +32,8 @@ int	ft_init_player(t_mlx **mlx, double angle)
 	player->y = ft_pixel_player((*mlx)->y_player, *mlx);
 	player->fov = 60;
 	player->nb_rays = (*mlx)->width / (*mlx)->width_one_wall;
-	player->speed = 0.5;
-	player->angle_speed = 1;
+	player->speed = 2;
+	player->angle_speed = 2;
 	player->angle = angle;
 	player->w = 0;
 	player->s = 0;
