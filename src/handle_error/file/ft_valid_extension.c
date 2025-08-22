3b7bf0c15580a6fb020b4ab583e66a5b1b39c7c0
Ivan Tahiana Rakotomanga <irakotom@c1r11s6.42antananarivo.mg>
2025-08-22 19:56:01 +0300

    fix cache file

--- src/handle_error/file/ft_valid_extension.c
+++ src/handle_error/file/ft_valid_extension.c
@@ -12,7 +12,7 @@
 
 #include "../../../includes/header.h"
 
-void	ft_display_error_extenstion(char *extension)
+void	ft_display_error_extension(char *extension)
 {
 	ft_putstr_fd("Error\nInvalid file extension", 2);
 	ft_putstr_fd(",it must end with '", 2);
@@ -25,22 +25,25 @@ int	ft_valid_extension(char *file, char *extension)
 	int	len_file;
 	int	len_extension;
 
-	len_file = ft_strlen(file);
-	len_extension = ft_strlen(extension);
-	len_file--;
-	len_extension--;
+	len_file = ft_strlen(file) -1;
+	len_extension = ft_strlen(extension) -1;
 	while (file[len_file] && extension[len_extension])
 	{
 		if (extension[len_extension])
 		{
 			if (file[len_file] != extension[len_extension])
 			{
-				ft_display_error_extenstion(extension);
+				ft_display_error_extension(extension);
 				return (0);
 			}
 		}
 		len_file--;
 		len_extension--;
+	}
+	if(file[len_file] == '\0' || file[len_file] == '/')
+	{
+		ft_display_error_extension(extension);
+				return (0);
 	}
 	return (1);
 }
